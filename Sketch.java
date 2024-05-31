58270cbb6609090d0755bfe1eaa1a24cef94bbb8
EdricLai <edric.lai25@ycdsbk12.ca>
2024-05-31 11:32:58 -0400

    added lightning

--- Sketch.java
+++ Sketch.java
@@ -13,12 +13,14 @@ public class Sketch extends PApplet {
   */
   class ObjEnemy {
     // variables
+    int intHealth;
     float fltPosX;
     float fltPosY;
     float fltDia;
     float fltSpeed;
     // constructor
-    public ObjEnemy (float posX, float posY, float dia, float speed) {
+    public ObjEnemy (int health, float posX, float posY, float dia, float speed) {
+      this.intHealth = health;
       this.fltPosX = posX;
       this.fltPosY = posY;
       this.fltDia = dia;
@@ -33,12 +35,12 @@ public class Sketch extends PApplet {
   // data structures
   ArrayList<ObjEnemy> arrListEnemy = new ArrayList<ObjEnemy>();
   // general
-  int intTimerInput;
   float fltSize;
-  float fltSizeStroke;
   float fltGuiX;
   float fltGuiY;
   // inputs
+  int intDelayTimer;
+  int intInputTimer;
   String strInputs;
   boolean boolW;
   boolean boolA;
@@ -60,12 +62,12 @@ public class Sketch extends PApplet {
   public void setup() {
     // initialize variables
     fltSize = width * height;
-    fltSizeStroke = fltSize / 200000;
     fltGuiX = 0 + fltSize / 5000;
     fltGuiY = 0 + fltSize / 5000;
     strInputs = "";
     // general
     background(0, 0, 0);
+    strokeWeight(fltSize / 200000);
     textSize(fltSize / 20000);
     // tests
     enemyInit();
@@ -104,17 +106,19 @@ public class Sketch extends PApplet {
   private void enemyInit() {
     // local variables
     float numEnemies = random(1, 10);
+    int health;
     float posX;
     float posY;
     float dia;
     float speed;
     // initialize
     for (int i = 0; i < numEnemies; i++) {
+      health = 20;
       posX = random(width);
       posY = 0;
       dia = random(fltSize / 100000, fltSize / 10000);
       speed = random(fltSize / 200000, fltSize / 100000);
-      arrListEnemy.add(new ObjEnemy(posX, posY, dia, speed));
+      arrListEnemy.add(new ObjEnemy(health, posX, posY, dia, speed));
     }
   }
 
@@ -123,42 +127,33 @@ public class Sketch extends PApplet {
    * inputs
   */
   private void inputsMain() {
-    // displays record
+    // general
+    intDelayTimer -= 1;
+    intInputTimer -= 1;
     text(strInputs, fltGuiX, fltGuiY);
-    intTimerInput -= 1;
-    if (keyPressed && intTimerInput <= 0) {
-      // records inputs
-      if (boolW) {
-        strInputs += 'w';
+    // records inputs
+    if (strInputs.length() < 3 && keyPressed && intInputTimer <= 0) {
+      if (boolW) {strInputs += 'w';}
+      if (boolA) {strInputs += 'a';}
+      if (boolS) {strInputs += 's';}
+      if (boolD) {strInputs += 'd';}
+      intInputTimer = 10;
+    }
+    // when record reaches limit
+    if (strInputs.length() >= 3) {
+      // input combos
+      if (strInputs.equals("wws")) {
+        meteor();
       }
-      if (boolA) {
-        strInputs += 'a';
+      else if (strInputs.equals("sss")) {
+        if (intDelayTimer < 0) {intDelayTimer = 40;}
+        if (intDelayTimer % 20 == 0) {
+          lightning(mouseX, mouseY);
+        }
       }
-      if (boolS) {
-        strInputs += 's';
-      }
-      if (boolD) {
-        strInputs += 'd';
-      }
-      intTimerInput = 10;
-      // when record reaches limit
-      if (strInputs.length() >= 3) {
-        // input combos
-        if (strInputs == "wws") {
-          meteor();
-        }
-        else if (strInputs == "sss") {
-          lightning();
-        }
-        else if (strInputs == "daa") {
-          push();
-        }
-        else if (strInputs == "add") {
-          arrows();
-        }
-        // resets record
+      // resets record
+      if (intDelayTimer <= 0) {
         strInputs = "";
-        intTimerInput = 20;
       }
     }
   }
@@ -168,18 +163,10 @@ public class Sketch extends PApplet {
    * keyPressed function
   */
   public void keyPressed() {
-    if (key == 'w') {
-      boolW = true;
-    }
-    else if (key == 'a') {
-      boolA = true;
-    }
-    else if (key == 's') {
-      boolS = true;
-    }
-    else if (key == 'd') {
-      boolD = true;
-    }
+    if (key == 'w') {boolW = true;}
+    else if (key == 'a') {boolA = true;}
+    else if (key == 's') {boolS = true;}
+    else if (key == 'd') {boolD = true;}
   }
 
   /**
@@ -187,18 +174,10 @@ public class Sketch extends PApplet {
    * keyReleased function
   */
   public void keyReleased() {
-    if (key == 'w') {
-      boolW = false;
-    }
-    else if (key == 'a') {
-      boolA = false;
-    }
-    else if (key == 's') {
-      boolS = false;
-    }
-    else if (key == 'd') {
-      boolD = false;
-    }
+    if (key == 'w') {boolW = false;}
+    else if (key == 'a') {boolA = false;}
+    else if (key == 's') {boolS = false;}
+    else if (key == 'd') {boolD = false;}
   }
 
   /**
@@ -212,24 +191,38 @@ public class Sketch extends PApplet {
   /**
    * called on command
    * lightning
+   * @param strikePosX  X coord where lightning lands
+   * @param strikePosY  Y coord where lightning lands
   */
-  private void lightning() {
-    //   
-  }
-
-  /**
-   * called on command
-   * push
-  */
-  private void push() {
-    //
-  }
-
-  /**
-   * called on command
-   * arrows
-  */
-  private void arrows() {
-    //
+  private void lightning(float strikePosX, float strikePosY) {
+    // local variables
+    float rposX = strikePosX;
+    float rposY = 0;
+    float rposX2 = 0;
+    float rposY2 = 0;
+    float bposX = 0;
+    float bposY = 0;
+    float bposX2 = 0;
+    float bposY2 = 0;
+    float space = fltSize / 10000;
+    // 1-5 main roots
+    for (int roots = 0; roots < random(1, 5); roots++) {
+      rposX2 = rposX + random(-space, space);
+      rposY2 = rposY + random(strikePosY / 3, strikePosY / 2);
+      stroke(0, 255, 255);
+      line(rposX, rposY, rposX2, rposY2);
+      rposX = rposX2;
+      rposY = rposY2;
+      bposX = rposX;
+      bposY = rposY;
+      // 1-5 branches
+      for (int branches = 0; branches < random(1, 5); branches++) {
+        bposX2 = bposX + random(-space, space);
+        bposY2 = bposY + random(-space, space);
+        line(bposX, bposY, bposX2, bposY2);
+        bposX = bposX2;
+        bposY = bposY2;
+      }
+    }
   }
 }
